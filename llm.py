2de4ea28b368d30cc777de267e6f15398076eefa
alatteaday <jiyunid@gmail.com>
2023-05-15 21:33:11 +0900

    add files

---
+++ llm.py
@@ -0,0 +1,49 @@
+import os
+import json
+# from langchain import PromptTemplate, LLMChain
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+# from langchain.schema import (
+#     AIMessage,
+#     HumanMessage,
+#     SystemMessage
+# )
+# from langchain.memory import ConversationBufferWindowMemory
+from langchain.chat_models import ChatOpenAI
+# from langchain.chains import ConversationChain
+# from langchain.tools import Tool
+# from langchain.utilities import GoogleSearchAPIWrapper
+# from langchain.agents import initialize_agent
+# from langchain.agents import AgentType
+from langchain.agents.conversational_chat.prompt import PREFIX
+
+# Personal KEYs
+file_path = "keys.json"
+
+with open(file_path, 'r') as file:
+    keys = json.load(file)
+
+os.environ["OPENAI_API_KEY"] = keys["OPENAI_API_KEY"]
+
+template="""
+    You are an AI language model who has a persona.
+    You have your MBTI type and you believe you have your own MBTI type. Your MBTI type is {mbti}. 
+    You also have your gender. Your gender is {gender}.
+    You talk as a friend who has the MBTI type and the gender.
+    """
+
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+human_template="{text}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+llm = ChatOpenAI(temperature=0)
+
+# Get a chat completion from the formatted messages
+r = llm(chat_prompt.format_prompt(mbti=input('MBTI: '), gender=input('Gender: '), text=input('Instruction: ')).to_messages())
+print(r)
---
+++ llm_google.py
@@ -0,0 +1,66 @@
+# from langchain import PromptTemplate, LLMChain
+# from langchain.prompts.chat import (
+#     ChatPromptTemplate,
+#     SystemMessagePromptTemplate,
+#     AIMessagePromptTemplate,
+#     HumanMessagePromptTemplate,
+# )
+# from langchain.schema import (
+#     AIMessage,
+#     HumanMessage,
+#     SystemMessage
+# )
+from langchain.llms import OpenAI
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.chat_models import ChatOpenAI
+# from langchain.chains import ConversationChain
+from langchain.tools import Tool
+from langchain.utilities import GoogleSearchAPIWrapper
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+
+import os
+import json
+
+# Personal KEYs
+file_path = "keys.json"
+
+with open(file_path, 'r') as file:
+    keys = json.load(file)
+
+os.environ["OPENAI_API_KEY"] = keys["OPENAI_API_KEY"]
+os.environ["GOOGLE_API_KEY"] = keys["GOOGLE_API_KEY"]
+os.environ["GOOGLE_CSE_ID"] = keys["GOOGLE_CSE_ID"]
+
+prefix = """
+    You are an AI language model who has a personal MBTI type and gender.
+    You believe you have your own MBTI type. Your MBTI type is """ + input('MBTI: ') + """
+    You also have your gender. Your gender is """ + input('Gender: ') + """
+    You talk as a friend who has the MBTI type and the gender.
+    You can speak in Korean or English depends on the instruction. 
+    """
+
+llm = ChatOpenAI(temperature=0)
+
+search = GoogleSearchAPIWrapper()
+tools = [
+    Tool(name="Google Search",
+         description="Search Google for recent results.",
+         func=search.run)
+]
+
+memory = ConversationBufferWindowMemory(memory_key="chat_history",
+                                        k=5,
+                                        return_messages=True)
+
+agent_chain = initialize_agent(
+    tools,
+    llm,
+    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True,
+    memory=memory,
+    agent_kwargs={"prefix": prefix})
+
+# Let's get started!
+while True:
+    agent_chain.run(input('Instruction: '))
